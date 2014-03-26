1fae8c681f6fe83d6fa78dcad58c5c9fc2d28d9d
Stanislav Venzerul <stanislav.venzerul@gmail.com>
2014-03-26 15:20:05 -0400

    bump version

--- dist/js/ng-breadcrumbs.js
+++ dist/js/ng-breadcrumbs.js
@@ -1,9 +1,11 @@
 /**
- * ng-breadcrumb.js - v0.0.5 - A better AngularJS service to help with breadcrumb-style navigation between views
+ * ng-breadcrumb.js - v0.0.6 - A better AngularJS service to help with breadcrumb-style navigation between views
  * Based on https://github.com/angular-app/angular-app/blob/master/client/src/common/services/breadcrumbs.js
  *
  * @author Ian Kennington Walter (http://www.ianvonwalter.com)
  */
+
+/* global angular */
 angular
   .module('ng-breadcrumbs', [])
   .factory('breadcrumbs', ['$rootScope', '$location', '$route', function ($rootScope, $location, $route) {
@@ -37,7 +39,7 @@ angular
             if (re.test(route)) {
               param = value;
             }
-            route = route.replace(re, ':' + key);
+            if (value) route = route.replace(re, ':' + key);
           });
           return { path: route, param: param };
         };
